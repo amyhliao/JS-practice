7237746faf660d7bc154faf38fa998a6d953e96a
Amy Liao <amyhliao88@gmail.com>
2016-01-06 16:36:32 -0800

    add variables and constants to phone purchase

--- phonepurchase.js
+++ phonepurchase.js
@@ -6,3 +6,9 @@ var ACCESSORY = 20;
 var available_funds = 1000.01;
 var cost = 0;
 
+// purchase of phones while funds are still avail
+    // if there are funds to buy accessories
+
+// need to incorporate sales tax
+
+// checks if the phone is affordable; enough money in avaialable funds
